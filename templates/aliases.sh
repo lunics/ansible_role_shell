9a23341f6fe309ff45291414feec0d6b95698d05
lunics <contact@lunics.fr>
2023-06-16 13:49:41 +0200

    aliases OK

--- templates/aliases.sh
+++ templates/aliases.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
-{% for item in aliases | dict2items(key_name="alias", value_name="command") %}
+{% for item in item[1] | dict2items(key_name="alias", value_name="command") %}
 alias {{ item.alias }}=" {{ item.command }} "
 {% endfor %}
--- templates/exports.sh
+++ templates/exports.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 {% for item in exports | dict2items(key_name="export", value_name="value") %}
   {% if item.value | regex_search("\$\(") %}    {# when item.value start by $(command) then don't add quotes #}
 export {{ item.export }}={{ item.value }}
