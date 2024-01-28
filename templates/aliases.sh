5a5a1f2bd40f1cda561550a8f41b33ee48eaefd3
lunics <mail@lunics.fr>
2024-01-28 15:29:38 +0100

    OK aliases.yml

--- templates/aliases.sh
+++ templates/aliases.sh
@@ -1,3 +1,3 @@
-{% for item in item[1] | dict2items(key_name="alias", value_name="command") %}
+{% for item in shell_aliases | dict2items(key_name="alias", value_name="command") %}
 alias {{ item.alias }}=" {{ item.command }} "
 {% endfor %}
