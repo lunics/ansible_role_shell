8097d0b2af23dc642c0b02c5ced0370f61a31038
lunics <mail@lunics.fr>
2024-02-05 21:47:35 +0100

    fix user dict architecture

--- templates/sources.sh
+++ templates/sources.sh
@@ -2,14 +2,10 @@
 
 source $HOME/{{ path_shell }}/exports.sh
 source $HOME/{{ path_shell }}/aliases.sh
-{# % if item.shell is defined and (item.shell == "zsh" or item.shell.type == "zsh") %#}
-{% if item.shell is defined %}
-{%   if item.shell.type == "zsh" %}
+{% if item.shell == "zsh" %}
 source $HOME/{{ path_zsh  }}/zshrc
-{#% elif item.shell is not defined or item.shell == "bash" or item.shell.type == "zsh" %#}
-{%   elif item.shell.type == "bash" %}
+{% elif item.shell == "bash" %}
 source $HOME/{{ path_bash }}/bashrc
-{%   endif %}
 {% endif %}
 
 for file in $HOME/{{ path_shell }}/functions/*; do
